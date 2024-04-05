8317181b812adaee120179a86e60f35380b9698f
roxootiago <tiago.roxo@hotmail.com>
2024-04-05 19:56:11 -0300

    aaaaaaa

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = "cool-cache";
 
 // Add whichever assets you want to pre-cache here:
-const PRECACHE_ASSETS = [ "./offline.html","./assets/"];
+const PRECACHE_ASSETS = "./offline.html"
 
 // Listener for the install event - pre-caches our assets list on service worker install.
 self.addEventListener("install", (event) => {
