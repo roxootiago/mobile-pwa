e385263c060ce81191aaf2552a8722d01728aa18
roxootiago <tiago.roxo@hotmail.com>
2024-04-05 18:58:01 -0300

    fix: corrigindo bugs do offlie

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+var cacheName = "pwaTeste+-v1.0";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) =>
+        cache.addAll([
+          "./offline.html",
+          "./assets/css/main.css",
+          "assets/vendor/bootstrap/css/bootstrap.min.css",
+          "assets/vendor/bootstrap-icons/bootstrap-icons.css",
+          "assets/vendor/aos/aos.css",
+          "assets/vendor/glightbox/css/glightbox.min.css",
+          "assets/vendor/swiper/swiper-bundle.min.css",
+        ])
+      )
+  );
+});
+
+self.addEventListener("message", function (event) {
+  if (event.data.action === "skipWaiting") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener("fetch", function (event) {
+  //Atualizacao internet
+  event.respondWith(
+    (async function () {
+      try {
+        return await fetch(event.request);
+      } catch (err) {
+        return caches.match(event.request);
+      }
+    })()
+  );
+
+  //Atualizacao cache
+  /*event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );*/
+});
