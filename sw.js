49e0158d9bee9e1e47674a6b87a55c314d6554db
roxootiago <tiago.roxo@hotmail.com>
2024-04-06 00:57:13 -0300

    feat: atualizando sw

--- sw.js
+++ sw.js
@@ -1,37 +1,49 @@
-const CACHE_NAME = "cool-cache";
+// This is the "Offline page" service worker
 
-// Add whichever assets you want to pre-cache here:
-const PRECACHE_ASSETS = ["./offline.html"];
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+);
 
-// Listener for the install event - pre-caches our assets list on service worker install.
-self.addEventListener("install", (event) => {
+const CACHE = "./offline.html";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener("install", async (event) => {
   event.waitUntil(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      cache.addAll(PRECACHE_ASSETS);
-    })()
+    caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(async () => {
-    const cache = await caches.open(CACHE_NAME);
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResp = await event.preloadResponse;
 
-    // match the request to our cache
-    const cachedResponse = await cache.match(event.request);
+          if (preloadResp) {
+            return preloadResp;
+          }
 
-    // check if we got a valid response
-    if (cachedResponse !== undefined) {
-      // Cache hit, return the resource
-      return cachedResponse;
-    } else {
-      // Otherwise, go to the network
-      return fetch(event.request);
-    }
-  });
+          const networkResp = await fetch(event.request);
+          return networkResp;
+        } catch (error) {
+          const cache = await caches.open(CACHE);
+          const cachedResp = await cache.match(offlineFallbackPage);
+          return cachedResp;
+        }
+      })()
+    );
+  }
 });
