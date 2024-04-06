8cecf1328b6c436416dc7bfded81cd981b71245e
roxootiago <tiago.roxo@hotmail.com>
2024-04-06 01:09:25 -0300

    feat: colocando o cache add all

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@ self.addEventListener("message", (event) => {
 
 self.addEventListener("install", async (event) => {
   event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
+    caches.open(CACHE).then((cache) => cache.addAll(offlineFallbackPage))
   );
 });
 
